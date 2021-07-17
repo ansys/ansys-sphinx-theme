5dfff7e8d12c9477b29e5a96032021ea8b547522
Alex Kaszynski <akascap@gmail.com>
2021-07-17 15:04:11 -0600

    bump to 0.1.2

--- pyansys_sphinx_theme/_version.py
+++ pyansys_sphinx_theme/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 0, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 1, 1
+version_info = 0, 1, 2
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
