6b528a75f8c6a0e51fc1fc1202bed0bf7de19473
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-05-23 15:57:03 +0200

    Add requirements to toml file

--- src/pyansys_sphinx_theme/latex/__init__.py
+++ src/pyansys_sphinx_theme/latex/__init__.py
@@ -1,17 +1,21 @@
 """This is module to create coverpage to the latex pdf."""
 import os
+from pathlib import Path
 
 import jinja2
+
+LATEX_SUBPKG = Path(os.path.dirname(os.path.abspath(__file__)))
+COVER_TEX = "cover.tex"
 
 
 def generate_preamble(title, watermark="watermark"):
     """Return the latex macrosfor title page."""
-    variables = dict(Project_title=title, Watermark=watermark)
+    variables = dict(_title=title, _watermark=watermark)
 
     latex_jinja_env = jinja2.Environment(
         block_start_string="\BLOCK{",
         block_end_string="}",
-        variable_start_string="\VAR{",
+        variable_start_string="\pyvar{",
         variable_end_string="}",
         comment_start_string="\#{",
         comment_end_string="}",
@@ -19,7 +23,7 @@ def generate_preamble(title, watermark="watermark"):
         line_comment_prefix="%#",
         trim_blocks=True,
         autoescape=False,
-        loader=jinja2.FileSystemLoader(os.path.dirname(os.path.realpath(__file__))),
+        loader=jinja2.FileSystemLoader(LATEX_SUBPKG),
     )
-    template = latex_jinja_env.get_template("cover.tex")
+    template = latex_jinja_env.get_template(COVER_TEX)
     return template.render(variables)
