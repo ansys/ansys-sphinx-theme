2f667f4a3601bff48e14e71d895184e8e9a16453
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-05-24 10:17:14 +0200

    Update year to the preamble

--- src/pyansys_sphinx_theme/latex/__init__.py
+++ src/pyansys_sphinx_theme/latex/__init__.py
@@ -1,16 +1,19 @@
 """This module creates the title page for a LaTeX pdf."""
+from datetime import datetime
 import os
 from pathlib import Path
 
 import jinja2
 
-LATEX_SUBPKG = Path(os.path.dirname(os.path.abspath(__file__)))
-COVER_TEX = "cover.tex"
+LATEX_SUBPKG = Path(os.path.dirname(os.path.realpath(__file__)))
+COVER_TEX = LATEX_SUBPKG / "cover.tex"
 
 
-def generate_preamble(title, watermark="watermark"):
-    """Return the latex macrosfor title page."""
-    variables = dict(_title=title, _watermark=watermark)
+def generate_preamble(title, watermark="watermark", year=None):
+    """Return the latex macros for the title page."""
+    if year is None:
+        year = datetime.today().year
+    variables = dict(_title=title, _watermark=watermark, _year=year)
 
     latex_jinja_env = jinja2.Environment(
         block_start_string="\BLOCK{",
@@ -23,7 +26,7 @@ def generate_preamble(title, watermark="watermark"):
         line_comment_prefix="%#",
         trim_blocks=True,
         autoescape=False,
-        loader=jinja2.FileSystemLoader(LATEX_SUBPKG),
+        loader=jinja2.FileSystemLoader(COVER_TEX),
     )
-    template = latex_jinja_env.get_template(COVER_TEX)
+    template = latex_jinja_env.get_template(".")
     return template.render(variables)
