b5d429bbf789fb4f7206c07c296e6bb0008d92a5
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-02-14 13:27:25 +0100

    Update src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/table.js

--- src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/table.js
+++ src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/table.js
@@ -1,11 +1,3 @@
 $(document).ready( function () {
     $('table.datatable').DataTable();
 } );
-
-$(document).ready( function () {
-    $('table.gridtable').GridTable();
-} );
-
-$(document).ready( function () {
-    $('table.gianttables').GiantTable();
-} );
