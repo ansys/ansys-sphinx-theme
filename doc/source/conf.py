25340caa12c7861ad150f8e5384d4922bea189eb
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-02-27 17:01:50 +0100

    VER : version 0.9.5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -61,7 +61,7 @@ html_theme_options = {
         },
     ],
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
     },
     "use_meilisearch": {
