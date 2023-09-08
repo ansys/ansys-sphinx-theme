86f40293c1749f41b31b93793efb704bf81093df
Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
2023-09-08 12:20:52 +0200

    ver: bump version 0.11.1

--- src/ansys_sphinx_theme/__init__.py
+++ src/ansys_sphinx_theme/__init__.py
@@ -11,7 +11,7 @@ from sphinx.application import Sphinx
 from ansys_sphinx_theme.extension.linkcode import DOMAIN_KEYS, sphinx_linkcode_resolve
 from ansys_sphinx_theme.latex import generate_404  # noqa: F401
 
-__version__ = "0.11.0"
+__version__ = "0.11.1"
 
 
 # Declare the fundamental paths of the theme
