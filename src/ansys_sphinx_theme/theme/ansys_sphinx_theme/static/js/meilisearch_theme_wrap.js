cc7ab154eb6b6f7bd5050494e668c80ba3c634cc
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-10-09 17:27:58 +0200

    doc: update trim space

--- src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/meilisearch_theme_wrap.js
+++ src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/meilisearch_theme_wrap.js
@@ -22,32 +22,35 @@ require(["docsSearchBar"], function (docsSearchBar) {
   // Function to show the magnifier icon
   function showMagnifierIcon() {
     var searchIcon = document.getElementById("search-icon");
-    searchIcon.classList.remove("fa-solid", "fa-spinner", "fa-spin");
+    searchIcon.classList.remove("fa-spinner", "fa-spin"); // Remove spinner classes
+    searchIcon.classList.add("fa-magnifying-glass"); // Add magnifier icon class
   }
 
   // Function to show the spinner icon
   function showSpinnerIcon() {
     var searchIcon = document.getElementById("search-icon");
     if (searchIcon) {
-      searchIcon.classList = "fa-solid fa-spinner fa-spin";
+      searchIcon.classList.remove("fa-magnifying-glass"); // Remove magnifier icon class
+      searchIcon.classList.add("fa-spinner", "fa-spin"); // Add spinner classes
     }
   }
 
   document
     .getElementById("search-bar-input")
     .addEventListener("input", function () {
-      // Show the spinner icon when the user starts typing
-      const suggestions = document.querySelectorAll(".dsb-suggestion");
-      const noSuggestions = suggestions.length === 0;
-
-      if (noSuggestions) {
-        // Show the spinner icon only when there are no suggestions
+      const inputValue = this.value.trim(); // Trim whitespace from input value
+      // Show the spinner icon only when there is input and no suggestions
+      if (
+        inputValue &&
+        document.querySelectorAll(".dsb-suggestion").length === 0
+      ) {
         showSpinnerIcon();
       } else {
         // Hide the spinner icon when there are suggestions
         showMagnifierIcon();
       }
     });
+
   // Listen for changes in the dropdown selector and update the index uid and suggestion accordingly
   document
     .getElementById("indexUidSelector")
