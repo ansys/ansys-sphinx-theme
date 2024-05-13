dfb569471bbead8b6b5c5ab06906eb8a7c98c010
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-05-13 17:25:18 +0200

    docs: remove the docs

--- src/ansys_sphinx_theme/__init__.py
+++ src/ansys_sphinx_theme/__init__.py
@@ -95,6 +95,23 @@ def get_version_match(semver: str) -> str:
     return ".".join([major, minor])
 
 
+def get_autoapi_templates_dir_relative_path(path: pathlib.Path) -> str:
+    """Return a string representing the relative path for autoapi templates.
+
+    Parameters
+    ----------
+    path : pathlib.Path
+        Path to the desired file.
+    Returns
+    -------
+    str
+        A string rerpesenting the relative path to the autoapi templates.
+    """
+    return os.path.relpath(
+        str(AUTOAPI_TEMPLATES_PATH.absolute()), start=str(path.parent.absolute())
+    )
+
+
 def convert_version_to_pymeilisearch(semver: str) -> str:
     """Convert a semantic version number to pymeilisearch-compatible format.
 
