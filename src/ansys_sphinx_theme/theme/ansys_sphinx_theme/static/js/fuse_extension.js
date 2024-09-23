809533c358bb5b91af21de9fb592eb8f287c378b
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-09-23 09:53:45 +0200

    fix: add switch case condition

--- src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/fuse_extension.js
+++ src/ansys_sphinx_theme/theme/ansys_sphinx_theme/static/js/fuse_extension.js
@@ -2,25 +2,31 @@ const searchBar = document.getElementById("search-bar");
 const searchBarBox = searchBar.querySelector(".bd-search input");
 resultsContainer = document.getElementById("results");
 
-// Expand search box on click
-searchBarBox.addEventListener("click", function () {
+// Function to expand the search bar and display results
+function expandSearchBox() {
   searchBarBox.classList.add("expanded");
   searchBarBox.focus();
 
   if (searchBarBox.value.trim().length >= parseInt(min_chars_for_search)) {
     resultsContainer.style.display = "flex";
   }
-});
+}
 
-// Expand the search box on pressing ctrl + k
+// Expand search box on click
+searchBarBox.addEventListener("click", expandSearchBox);
+
+// Keydown event handler using switch
 document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey && event.key === "k") {
-    searchBarBox.classList.add("expanded");
-    searchBarBox.focus();
-
-    if (searchBarBox.value.trim().length >= parseInt(min_chars_for_search)) {
-      resultsContainer.style.display = "flex";
-    }
+  switch (event.key) {
+    case "k":
+      if (event.ctrlKey) {
+        expandSearchBox();
+      }
+      break;
+    case "Escape":
+      resultsContainer.style.display = "none";
+      searchBarBox.classList.remove("expanded");
+      break;
   }
 });
 
@@ -30,14 +36,6 @@ document.addEventListener("click", function (event) {
     !resultsContainer.contains(event.target) &&
     event.target !== searchBarBox
   ) {
-    resultsContainer.style.display = "none";
-    searchBarBox.classList.remove("expanded");
-  }
-});
-
-// Close the result container on pressing the escape key
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
     resultsContainer.style.display = "none";
     searchBarBox.classList.remove("expanded");
   }
