bb4aa314a20476b0d2b88c910573f073192a6085
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-09-30 08:44:37 +0200

    fix: cleanup code

--- src/ansys_sphinx_theme/search/fuse_search.py
+++ src/ansys_sphinx_theme/search/fuse_search.py
@@ -96,30 +96,24 @@ class SearchIndex:
             if doc_part in self.env.titles
         ]
 
-        self.breadcrumbs_title = " > ".join(title_string)
+        breadcrumbs_title = " > ".join(title_string)
 
-        if self.breadcrumbs_title:
-    # Join the titles with " > "
-    breadcrumbs = " > ".join(title_parts)
-    
-    # Check whether to avoid including duplicated items
-    ignore_doc_title = self.section_title == self.doc_title
+        # Check whether to avoid including duplicated items
+        ignore_doc_title = self.section_title == self.doc_title
 
-    # Determine the final breadcrumb output
-    if ignore_doc_title:
-        breadcrumb = f"{breadcrumbs} > {section_title}" if breadcrumbs else section_title
-    else:
-        breadcrumb = f"{breadcrumbs} > {self.doc_title} > {section_title}" if breadcrumbs else f"{self.doc_title} > {section_title}"
-    
-    return breadcrumb
-                return f"{self.breadcrumbs_title} > {section_title}"
-            else:
-                return f"{self.breadcrumbs_title} > {self.doc_title} > {section_title}"
-        return (
-            section_title
-            if self.section_title == self.doc_title
-            else f"{self.doc_title} > {section_title}"
-        )
+        # Determine the final breadcrumb output
+        if ignore_doc_title:
+            breadcrumb = (
+                f"{breadcrumbs_title} > {section_title}" if breadcrumbs_title else section_title
+            )
+        else:
+            breadcrumb = (
+                f"{breadcrumbs_title} > {self.doc_title} > {section_title}"
+                if breadcrumbs_title
+                else f"{self.doc_title} > {section_title}"
+            )
+
+        return breadcrumb
 
     @property
     def indices(self):
