024c9e2907c30b5cefd9de8716e88d5845d2890c
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2025-10-08 15:03:40 +0200

    fix: replace old logo

--- src/ansys_sphinx_theme/__init__.py
+++ src/ansys_sphinx_theme/__init__.py
@@ -87,12 +87,8 @@ pyansys_logo_white = str((LOGOS_PATH / "pyansys-logo-white-cropped.png").absolut
 watermark = str((LOGOS_PATH / "watermark.pdf").absolute())
 pyansys_logo_dark_mode = str((LOGOS_PATH / "pyansys_logo_transparent_white.png").absolute())
 pyansys_logo_light_mode = str((LOGOS_PATH / "pyansys_logo_transparent_black.png").absolute())
-ansys_part_of_synopsys_logo_light_mode = str(
-    (LOGOS_PATH / "ansys_logo_transparent_white.png").absolute()
-)
-ansys_part_of_synopsys_logo_dark_mode = str(
-    (LOGOS_PATH / "ansys_logo_transparent_black.png").absolute()
-)
+ansys_logo_light_mode = str((LOGOS_PATH / "ansys_logo_transparent_black.png").absolute())
+ansys_logo_dark_mode = str((LOGOS_PATH / "ansys_logo_transparent_white.png").absolute())
 
 
 def get_html_theme_path() -> pathlib.Path:
@@ -374,8 +370,8 @@ def configure_theme_logo(app: Sphinx):
     }
 
     ansys_logo = {
-        "image_dark": ansys_part_of_synopsys_logo_dark_mode,
-        "image_light": ansys_part_of_synopsys_logo_light_mode,
+        "image_dark": ansys_logo_dark_mode,
+        "image_light": ansys_logo_light_mode,
     }
     theme_options = app.config.html_theme_options
     logo_option = theme_options.get("logo")
